d5f8af462bdf2e09693dd2f7b97189481ee6d1ad
James Carroll <jamcar23@gmail.com>
2020-08-17 15:08:07 -0500

    feat(uploader): change uploader to upload full logs on 3G

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -105,7 +105,8 @@ class Uploader():
     self.last_exc = None
 
     self.immediate_priority = {"qlog.bz2": 0, "qcamera.ts": 1}
-    self.high_priority = {"rlog.bz2": 0, "fcamera.hevc": 1, "dcamera.hevc": 2}
+    self.medium_priority = {"rlog.bz2": 0}
+    self.high_priority = {"fcamera.hevc": 0, "dcamera.hevc": 1}
 
   def get_upload_sort(self, name):
     if name in self.immediate_priority:
@@ -147,8 +148,13 @@ class Uploader():
       if name in self.immediate_priority:
         return (key, fn)
 
+    # upload the full log files after immediate files, even on 3G
+    for name, key, fn in upload_files:
+      if name in self.medium_priority:
+        return (key, fn)
+
     if with_raw:
-      # then upload the full log files, rear and front camera files
+      # then, rear and front camera files
       for name, key, fn in upload_files:
         if name in self.high_priority:
           return (key, fn)
