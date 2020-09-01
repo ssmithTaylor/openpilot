6e6915baea48bd7619bf1e787b17188664dd4336
James Carroll <jamcar23@gmail.com>
2020-09-01 10:09:19 -0500

    feat(manager): attempt to reset to r2++

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -602,8 +602,8 @@ if __name__ == "__main__":
     if exit_status == 'reset':
       for _ in range(2):
         try:
-          subprocess.check_output(["git", "add", "-A", "&&", "git", "stash"], cwd=BASEDIR)
           subprocess.check_output(["git", "fetch"], cwd=BASEDIR)
+          subprocess.check_output(["git", "checkout", "r2++"], cwd=BASEDIR)
           subprocess.check_output(["git", "reset", "--hard", "@{u}"], cwd=BASEDIR)
           subprocess.check_output(["git", "checkout", "r2+"], cwd=BASEDIR)
           print('git reset successful!')
