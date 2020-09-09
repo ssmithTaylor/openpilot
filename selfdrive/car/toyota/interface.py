cb3a249535651e178ff34cf4d8ad23fe79e253d6
James Carroll <jamcar23@gmail.com>
2020-09-09 07:59:17 -0500

    fix(toyota): update steer ratio, tire stiffness, and actuator delay on corolla

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -197,8 +197,8 @@ class CarInterface(CarInterfaceBase):
       stop_and_go = True
       ret.safetyParam = 73
       ret.wheelbase = 2.63906
-      ret.steerRatio = 13.9
-      tire_stiffness_factor = 0.444  # not optimized yet
+      ret.steerRatio = 15.33
+      tire_stiffness_factor = 0.996  # not optimized yet
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
       #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
       #ret.lateralTuning.pid.kf = 0.00007818594
@@ -208,7 +208,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.outerLoopGain = 15.0
       ret.lateralTuning.indi.timeConstant = 5.5
       ret.lateralTuning.indi.actuatorEffectiveness = 6.0
-      ret.steerActuatorDelay = 0.57
+      ret.steerActuatorDelay = 0.6
 
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
       stop_and_go = True
