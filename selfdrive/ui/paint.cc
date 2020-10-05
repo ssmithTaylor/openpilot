844a6eec753c5c49a7a31d2541f1292e2d8b0cbf
James Carroll <jamcar23@gmail.com>
2020-10-05 09:51:47 -0500

    fix(ui): fix build issues with model and path data

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -316,7 +316,7 @@ static void update_all_lane_lines_data(UIState *s, const cereal::ModelData::Path
   update_lane_line_data(s, points, fmin(path.getStd(), 0.7), pstart + 1, path.getValidLen());
 }
 
-static void ui_draw_lane(UIState *s, const PathData *path, model_path_vertices_data *pstart, float prob) {
+static void ui_draw_lane(UIState *s, const cereal::ModelData::PathData *path, model_path_vertices_data *pstart, float prob) {
   float lane_pos = std::abs(path->poly[3]);  // get redder when line is closer to car
   float hue = 332.5 * lane_pos - 332.5;  // equivalent to {1.4, 1.0}: {133, 0} (green to red)
   hue = fmin(133, fmax(0, hue)) / 360.;  // clip and normalize
@@ -337,13 +337,13 @@ static void ui_draw_vision_lanes(UIState *s) {
 
   // Draw left lane edge
   ui_draw_lane(
-      s, &scene->model.getLeftLane(),
+      s, scene->model.getLeftLane(),
       pvd,
       scene->model.getLeftLane().getProb());
 
   // Draw right lane edge
   ui_draw_lane(
-      s, &scene->model.getRightLane(),
+      s, scene->model.getRightLane(),
       pvd + MODEL_LANE_PATH_CNT,
       scene->model.getRightLane().getProb());
 
@@ -352,7 +352,7 @@ static void ui_draw_vision_lanes(UIState *s) {
   }
 
   // Draw vision path
-  const float *p_poly = scene->model.path.poly;
+  const float *p_poly = scene->model.getPath().getPoly();
   ui_draw_track(s, false, &s->track_vertices[0], p_poly);
   if (scene->controls_state.getEnabled()) {
     // Draw MPC path when engaged
