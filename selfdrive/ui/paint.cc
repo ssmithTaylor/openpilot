732ece8429c9abd19355df85a290047cbadb28e0
James Carroll <jamcar23@gmail.com>
2020-10-05 10:27:39 -0500

    fix(ui): fix issues with poly data type

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -221,7 +221,7 @@ static void update_all_track_data(UIState *s) {
 }
 
 
-static void ui_draw_track(UIState *s, bool is_mpc, track_vertices_data *pvd, const float *p_poly) {
+static void ui_draw_track(UIState *s, bool is_mpc, track_vertices_data *pvd, const float* p_poly) {
  if (pvd->cnt == 0) return;
 
   nvgBeginPath(s->vg);
@@ -316,8 +316,8 @@ static void update_all_lane_lines_data(UIState *s, const cereal::ModelData::Path
   update_lane_line_data(s, points, fmin(path.getStd(), 0.7), pstart + 1, path.getValidLen());
 }
 
-static void ui_draw_lane(UIState *s, const cereal::ModelData::PathData *path, model_path_vertices_data *pstart, float prob) {
-  float lane_pos = std::abs(path->poly[3]);  // get redder when line is closer to car
+static void ui_draw_lane(UIState *s, const cereal::ModelData::PathData::Reader &path, model_path_vertices_data *pstart, float prob) {
+  float lane_pos = std::abs(path.getPoly()[3]);  // get redder when line is closer to car
   float hue = 332.5 * lane_pos - 332.5;  // equivalent to {1.4, 1.0}: {133, 0} (green to red)
   hue = fmin(133, fmax(0, hue)) / 360.;  // clip and normalize
   NVGcolor color = nvgHSLA(hue, 0.73, 0.64, prob * 255);
@@ -352,7 +352,7 @@ static void ui_draw_vision_lanes(UIState *s) {
   }
 
   // Draw vision path
-  const float *p_poly = scene->model.getPath().getPoly();
+  const float* p_poly = scene->model.getPath().getPoly();
   ui_draw_track(s, false, &s->track_vertices[0], p_poly);
   if (scene->controls_state.getEnabled()) {
     // Draw MPC path when engaged
