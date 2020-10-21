862a16f0448ac7b011125414899fd5cc1aa9d394
James Carroll <jamcar23@gmail.com>
2020-10-20 21:52:49 -0500

    fix(tools): fix various typos and timeouts

--- tools/scripts/transfer.py
+++ tools/scripts/transfer.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import requests
+import time
 
 from datetime import datetime, timedelta
 from common.api import Api
@@ -25,28 +26,40 @@ def transfer_route(route, tools_api, api, dongle_id):
             _, _, time_str, segment_num, fn = urlparse(url).path.rsplit('/', maxsplit=4)
             segment_file = f'{time_str}--{segment_num}/{fn}'
             print(f"Getting upload URL for: {segment_file}")
+            failed_last_run = False
 
 
-            url_resp = api.get("v1.3/" + dongle_id + "/upload_url/", timeout=10, path=segment_file, access_token=api.get_token())
-            url_resp_json = json.loads(url_resp.text)
-            upload_url = url_resp_json['url']
-            headers = url_resp_json['headers']
+            while True:
+                try:
+                    url_resp = api.get("v1.3/" + dongle_id + "/upload_url/", timeout=10, path=segment_file, access_token=api.get_token())
+                    url_resp_json = json.loads(url_resp.text)
+                    upload_url = url_resp_json['url']
+                    headers = url_resp_json['headers']
 
-            print(f"Uploading: {segment_file}")
-            upload_resp = requests.put(upload_url, data=dat, headers=headers, timeout=10)
+                    print(f"Uploading: {segment_file}")
+                    upload_resp = requests.put(upload_url, data=dat, headers=headers, timeout=10)
 
-            if upload_resp is not None and upload_resp.status_code in (200, 201):
-                print(f"Finished uploading: {segment_file}\n\n")
+                    if upload_resp is not None and upload_resp.status_code in (200, 201):
+                        print(f"Finished uploading: {segment_file}\n\n")
+                        break
+                except Exception as e:
+                    print(f"Error while uploading: {e}")
+
+                    if failed_last_run:
+                        raise e
+                    else:
+                        failed_last_run = True
+                        time.sleep(60)
+
 
 def get_uploaded_routes(dongle_id, tools_api):
     print(f"Getting uploaded segments for {dongle_id}")
-    start_time = (datetime.now() - timedelta(days=365)).timestamp() * 1000
-    seg_resp = tools_api.get(f"v1.1/{dongle_id}/segments?from={start_time}")
-    seg_resp_json = json.loads(seg_resp.text)
+    start_time = (datetime.today() + timedelta(days=-365)).timestamp() * 1000
+    seg_resp = tools_api.get(f"v1/devices/{dongle_id}/segments?from={start_time}")
 
     segs = dict()
 
-    for seg_json in seg_resp_json:
+    for seg_json in seg_resp:
         segs[seg_json["canonical_route_name"]] = None
 
     print("Finished getting segments\n\n")
@@ -80,11 +93,11 @@ def main(args):
         new_segs = get_uploaded_routes(dongle_id, tools_api)
 
         for seg in old_segs:
-            if seg in new_segs:
+            if not seg or seg in new_segs:
                 print(f"Skipping {seg} because a route already exists on new dongle.\n\n")
                 continue
 
-            transfer_route(route, tools_api, api, dongle_id)
+            transfer_route(seg, tools_api, api, dongle_id)
     
         
 
