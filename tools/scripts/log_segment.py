620f7a56001ca3df8bc8ad9b5c27775b0ffa5331
James Carroll <jamcar23@gmail.com>
2020-11-13 14:27:38 -0600

    feat(logs): add script to log segment to txt file

---
+++ tools/scripts/log_segment.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+import os
+import json
+
+from tools.lib.route import Route
+from tools.lib.logreader import LogReader
+
+def main(args):
+  r = Route(args[0])
+  lr = LogReader(r.log_paths()[int(args[1])])
+
+
+  with open(f"{args[0]}--{args[1]}.txt", "w") as f:
+    f.write('\n'.join([str(msg) for msg in lr]))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
